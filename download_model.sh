83ab89b65f20b6724c7ee03ba1bdda7eaf97b232
bangvo96 <vqbang1996@gmail.com>
2020-08-21 15:46:43 +0700

    BIB board detection

--- download_model.sh
+++ download_model.sh
@@ -1,3 +1,11 @@
-wget -O draco/models/weights.zip https://storage.googleapis.com/marketplace_ai_project/adult_content_recognition/BIB_detection/BIB_model/50k_full/weights.zip
-unzip draco/models/weights.zip -d draco/models/
-rm -rf draco/models/weights.zip
+# Download BIB recognition's weights
+mkdir draco/models/BIB_recognition
+wget -O draco/models/BIB_recognition/weights.zip https://storage.googleapis.com/marketplace_ai_project/adult_content_recognition/BIB_detection/BIB_model/50k_full/weights.zip
+unzip draco/models/BIB_recognition/weights.zip -d draco/models/BIB_recognition/
+rm -rf draco/models/BIB_recognition/weights.zip
+
+# Download BIB board detection's weights
+mkdir draco/models/BIB_board_detection
+wget -O draco/models/BIB_recognition/weights.zip https://storage.googleapis.com/marketplace_ai_project/adult_content_recognition/BIB_detection/BIB_model/50k_full/weights.zip
+unzip draco/models/BIB_recognition/weights.zip -d draco/models/BIB_recognition/
+rm -rf draco/models/BIB_recognition/weights.zip
