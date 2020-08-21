9c4d50f29eeb5cf3e24af9b5193c931758f6259c
bangvo96 <vqbang1996@gmail.com>
2020-08-21 16:33:08 +0700

    BIB board detection

--- predict/BIB_recognition.py
+++ predict/BIB_recognition.py
@@ -1,21 +1,22 @@
-import argparse
-from PIL import Image
-from vietocr.tool.predictor import Predictor
-from vietocr.tool.config import Cfg
+if __name__=="__main":
+    import argparse
+    from PIL import Image
+    from vietocr.tool.predictor import Predictor
+    from vietocr.tool.config import Cfg
 
-img_link = "/content/DT-CTD (13)_box_0.JPG"
+    img_link = "/content/DT-CTD (13)_box_0.JPG"
 
-# Set config
-config = Cfg.load_config_from_name('vgg_transformer')
-config['weights'] = './draco/models/weights/transformerocr.pth'
-config['device'] = 'cuda:0'
-config['predictor']['beamsearch']=False
+    # Set config
+    config = Cfg.load_config_from_name('vgg_transformer')
+    config['weights'] = './draco/models/weights/transformerocr.pth'
+    config['device'] = 'cuda:0'
+    config['predictor']['beamsearch']=False
 
-# Set predictor
-detector = Predictor(config)
+    # Set predictor
+    detector = Predictor(config)
 
-# Predict image and show BIB code
-img = Image.open(img_link)
-s = detector.predict(img)
-print(s)
-print()
+    # Predict image and show BIB code
+    img = Image.open(img_link)
+    s = detector.predict(img)
+    print(s)
+    print()
